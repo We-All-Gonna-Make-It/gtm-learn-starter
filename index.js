eb4a822dfcb3f6eec0866a91a2f8c28bc4cf864d
Merto <mertenercan@gmail.com>
2023-02-24 00:23:09 +0300

    -added js and dataLayer config

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 
 
+const buttons = document.getElementsByClassName("addCart")
+const test = () => {
+    console.log("test")
+    window.dataLayer.push({'event': 'testing31'});
+}
+buttons[1].addEventListener("click", test)
